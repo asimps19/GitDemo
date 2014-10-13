d810aab9e2dd88bb8296adcd14df110860b62cc5
asimps19 <shahid@Shahids-Mac.local>
2014-10-13 10:37:57 -0700

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by Asim on 10/13/14.
+//  Copyright (c) 2014 Powersoft19. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by Asim on 10/13/14.
+//  Copyright (c) 2014 Powersoft19. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
